3825265a492dc6e684d5d3229d9a2c5f06feaf01
Dean <ndw1212@gmail.com>
2015-08-19 12:50:10 +0800

    First Commit

---
+++ QuizProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Enviormental_Issues_Quiz_Program
+{
+    public partial class QuizProgram : Form
+    {
+        public QuizProgram()
+        {
+            InitializeComponent();
+        }
+    }
+}
